a9ddfc27ebdc402ed284b2e02a04af2380612dd2
niteshsainicoder <kumarsundratijara@gmail.com>
2024-08-19 21:56:01 +0530

    Add the layout for recent open files and output

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,15 @@ import Image from "next/image";
 import CodeEditor from "./Component/codeEditor";
 export default function Home() {
   return (
-    <main className="flex relative bg-cyan-100 min-h-screen flex-col items-center justify-between pt-3 px-2">
-  
+    <main className="flex relative h-screen bg-rose-200  items-start justify-evenly  ">
+      <div className="w-1/6 bg-black h-screen max-h-[500px] text-white "> this is recent file section</div>
+      <div className="h-fit min-h-[500px] bg-green-300 w-4/6 flex justify-center items-center">
       <CodeEditor />
-     
-    </main>
+      </div>
+
+      <div className="w-1/6 bg-black max-h-[500px] h-full text-white "> this is recent section</div>
+
+
+    </main >
   );
 }
