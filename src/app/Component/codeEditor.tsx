d31b114e78f459cf10a58b6e9ba0fbd5fce14b0e
niteshsainicoder <kumarsundratijara@gmail.com>
2024-08-19 21:54:56 +0530

    changing appearance of editor

--- src/app/Component/codeEditor.tsx
+++ src/app/Component/codeEditor.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Editor } from '@monaco-editor/react';
 import * as monacoEditor from 'monaco-editor';
 import React, { useRef, useState } from 'react';
@@ -33,24 +34,25 @@ const CodeEditor: React.FC = () => {
     };
 
     return (
-        <div className={`w-full relative max-h-[550px] h-[550px] transition duration-200 ${theme === 'light' ? 'bg-neutral-100 text-black' : 'bg-zinc-900 text-white'}`}>
-            <div className='flex items-center p-2 gap-5 mb-1'>
-                <h1 className='px-5 p-2 border rounded-lg'>{fileName}</h1>
+        <div className={`w-full max-w-[900px] relative max-h-[600px] h-full min-h-[500px]  transition duration-200 ${theme === 'light' ? 'bg-neutral-100 text-black' : 'bg-zinc-900 text-white'}`}>
+            <div className='flex items-center p-2 gap-5'>
+                <h1 className='px-2 p-2 w-40 text-center text-sm text-wrap overflow-hidden border rounded-lg'>{fileName}</h1>
                 <button
                     type='reset'
                     className={`rotate-90 text-2xl ${theme === 'vs-dark' ? 'hover:bg-neutral-800' : 'hover:bg-neutral-200'} rounded-full p-1`}
-                    onClick={() => setShowPopup(true)}  // Show the popup when the button is clicked
+                    onClick={() => setShowPopup(!showPopup)}  // Show the popup when the button is clicked
                 >
                     &#9998;
                 </button>
                 <button
                     onClick={() => setTheme(theme === 'vs-dark' ? 'light' : 'vs-dark')}
+                    className='text-sm'
                 >
                     Theme
                 </button>
                 <select
                     title='lang'
-                    className={`rounded-md border p-1 text-center ${theme === 'light' ? 'bg-neutral-100 text-black border-black ' : 'bg-neutral-900 border-white text-white'}`}
+                    className={`rounded-md text-sm border p-2 text-center ${theme === 'light' ? 'bg-neutral-100 text-black border-black ' : 'bg-neutral-900 border-white text-white'}`}
                     value={language}
                     onChange={handleLanguageChange}
                 >
@@ -68,9 +70,6 @@ const CodeEditor: React.FC = () => {
                 onMount={handleEditorDidMount}
                 onChange={handleEditorChange}
             />
-            <div>
-                <button onClick={() => alert(editorRef.current?.getValue())}>Show the value</button>
-            </div>
             {showPopup && (
                 <Popup
                     onClose={() => setShowPopup(false)}
