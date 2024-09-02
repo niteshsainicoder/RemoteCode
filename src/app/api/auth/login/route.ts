dafc1e8d1a917b738125ba90764b62210dfed7be
niteshsainicoder <kumarsundratijara@gmail.com>
2024-09-02 14:19:26 +0530

    routes updated

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -7,12 +7,8 @@ import { dbconnect } from "@/dbconfig/dbconnect";
 export async function POST(req: NextRequest) {
   try {
     await dbconnect();
-    console.log(req);
-    // Log raw request body for debugging
     const rawBody = await req.text();
-    console.log("Raw request body:", rawBody);
 
-    // Parse the raw body as JSON
     const body = JSON.parse(rawBody);
     const { username, password } = body;
 
@@ -20,21 +16,18 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ message: "Missing fields" }, { status: 400 });
     }
 
-    // Find the user by username
     const findUser = await User.findOne({ username });
 
     if (!findUser) {
       return NextResponse.json({ message: "User not found" }, { status: 400 });
     }
 
-    // Check if the password is correct
     const checkPassword = await bcrypt.compare(password, findUser.password);
 
     if (!checkPassword) {
       return NextResponse.json({ message: "Wrong password" }, { status: 400 });
     }
 
-    // Generate JWT token
     if (!process.env.JWT_SECRET) {
       return NextResponse.json(
         { message: "JWT Secret not set in environment variables" },
@@ -48,7 +41,7 @@ export async function POST(req: NextRequest) {
 
     // Create response and set cookie with JWT token
     const response = NextResponse.json(
-      { message: "Successfully authenticated" },
+      { message: "Successfully authenticated" ,user:findUser},
       { status: 200 }
     ); 
     response.cookies.set("token", token, {
@@ -61,7 +54,6 @@ export async function POST(req: NextRequest) {
 
     return response;
   } catch (error: any) {
-    console.error("Error during authentication:", error);
     return NextResponse.json(
       { message: "Server error", error: error.message },
       { status: 500 }
