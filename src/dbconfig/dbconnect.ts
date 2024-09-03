0eb2f75991c562fffc0928a370755bff5bc60398
niteshsainicoder <kumarsundratijara@gmail.com>
2024-09-03 11:52:46 +0530

    db connect file updated

--- src/dbconfig/dbconnect.ts
+++ src/dbconfig/dbconnect.ts
@@ -1,27 +1,35 @@
 import mongoose from "mongoose";
 
-
 // To prevent multiple connections
 let isConnected = false;
 
 if (process.env.MONGODB_URI === undefined) {
-  console.log("Missing MONGODB_URI environment variable",process.env.MONGODB_URI);
+  console.log(
+    "Missing MONGODB_URI environment variable",
+    process.env.MONGODB_URI
+  );
 }
+
 export const dbconnect = async () => {
   if (isConnected) {
     console.log("Using existing MongoDB connection");
-    return;
+    return mongoose.connection; // Return the existing connection
   }
 
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}`);
+    const db = await mongoose.connect(`${process.env.MONGODB_URI}`, {
+      serverSelectionTimeoutMS: 5000, // 5 seconds timeout
+    });
 
-    mongoose.connection.on('connected', () => console.log('connected'));
-    mongoose.connection.on('open', () => console.log('open'));
-    mongoose.connection.on('disconnected', () => console.log('disconnected'));
-    mongoose.connection.on('reconnected', () => console.log('reconnected'));
-    mongoose.connection.on('disconnecting', () => console.log('disconnecting'));
-    mongoose.connection.on('close', () => console.log('close'));
+    mongoose.connection.on("connected", () => console.log("MongoDB connected"));
+    mongoose.connection.on("open", () =>
+      console.log("MongoDB connection open")
+    );
+    mongoose.connection.on("close", () => {
+      console.log("MongoDB connection closed");
+      isConnected = false;
+    });
+
     return mongoose.connection;
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
