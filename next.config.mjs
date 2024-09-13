5a8c0cd509e4bb40274800246931426c6a651407
niteshsainicoder <kumarsundratijara@gmail.com>
2024-09-13 19:58:51 +0530

    cors problem solved

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*", // Apply to all API routes
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Allow requests from all origins
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
