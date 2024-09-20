72006be68ed77cc0b6f77977b6fe8e0bc1d8e29b
niteshsainicoder <kumarsundratijara@gmail.com>
2024-09-20 21:44:00 +0530

    updated

--- src/app/Component/nav.tsx
+++ src/app/Component/nav.tsx
@@ -18,14 +18,15 @@ const Nav = () => {
     <div className={`w-full ${theme == 'vs-dark' ? `bg-neutral-900 border-t border-b border-grey-400 text-neutral-200 ` : `bg-light border-t  border-black text-neutral-900`}  h-16 flex justify-evenly items-center `} >
       <div className="w-6/12 h-fit text-center  ">
         <p className="font-extrabold antialiased text-xl  shadow-slate-500 ">
-          <span className="text-3xl font-extrabold">
-            &#8475;
-          </span>
-          emote
+          
           <span className="text-2xl font-extrabold">
             &#264;
           </span>
           ode
+          <span className="text-3xl font-extrabold">
+            &#8475;
+          </span>
+         un
         </p>
       </div>
       <div className="w-4/12 h-fit  text-right flex justify-evenly gap-7 ">
