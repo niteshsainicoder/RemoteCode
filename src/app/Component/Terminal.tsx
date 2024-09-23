f281c449883abcdf00a02b7186990bd897fcf332
niteshsainicoder <kumarsundratijara@gmail.com>
2024-09-23 21:07:28 +0530

    tooltip and terminal updated

--- src/app/Component/Terminal.tsx
+++ src/app/Component/Terminal.tsx
@@ -1,8 +1,7 @@
 import { useTheme } from '@/Context/themecontext';
 import axios from 'axios';
-import { stringify } from 'querystring';
 import React, { useEffect, useState } from 'react';
-
+import { Tooltip } from "@nextui-org/tooltip";
 interface TerminalProps {
     title: string;
     data: string;
@@ -14,7 +13,7 @@ interface TerminalProps {
     classname: string;
 }
 
-const Terminal: React.FC<TerminalProps> = ({ title, data, isOpen, time, error, onToggle, loading, classname }) => {
+const Terminal: React.FC<TerminalProps> = ({ title, data, isOpen, time = 0.0, error, onToggle, loading, classname }) => {
     const { theme } = useTheme();
     const [serVerRunning, setServerRunning] = useState<boolean>(false)
     const [Time, setTime] = useState<string>('')
@@ -37,9 +36,11 @@ const Terminal: React.FC<TerminalProps> = ({ title, data, isOpen, time, error, o
     const datatime = (time: number) => {
         // If the time is less than 1 second, convert to milliseconds
         if (time < 1) {
-            return `${(time * 1000)} ms`;
+            console.log(time);
+
+            return `${Math.floor(time * 1000) / 1000} ms`;
         } else {
-            return `${time} s`;
+            return `${Math.floor(time * 100) / 100} s`;
         }
     }
 
@@ -70,7 +71,28 @@ const Terminal: React.FC<TerminalProps> = ({ title, data, isOpen, time, error, o
                 onClick={onToggle}
                 className='w-full h-10  cursor-pointer font-bold antialiased text-center pt-2   flex  justify-between px-9 items-center'
             >
-                {title} <sup className={`w-2 absolute left-28  top-3 rounded-full h-2 ${serVerRunning ? 'bg-green-500' : 'bg-red-500'}`}></sup>
+                {title}
+                <sup className={`w-2 absolute left-28  top-3 rounded-full h-2 ${serVerRunning ? 'bg-green-500' : 'bg-red-500'}`}>
+                    <Tooltip
+                        className='rounded-lg'
+                        showArrow
+                        placement="right"
+                        color={serVerRunning ? 'success' : 'warning'}
+                        content={serVerRunning ? 'Server Running' : 'Server Not Running'}
+                        classNames={{
+                            base: [
+                                // arrow color
+                                "before:bg-neutral-400 dark:before:bg-white",
+                            ],
+                            content: [
+                                "py-[6px] px-4 shadow-xl",
+                                "text-black bg-gradient-to-br from-white to-neutral-400",
+                            ],
+                        }}
+                    >
+                        <span className='text-transparent'>a</span>
+                    </Tooltip>
+                </sup>
                 <span
                     className={`inline-block ml-2 text-xl transform transition  ${isOpen ? 'rotate-180  pt-3' : 'rotate-0 pt-1'}`}
                 >
@@ -79,16 +101,20 @@ const Terminal: React.FC<TerminalProps> = ({ title, data, isOpen, time, error, o
             </p>
 
             <div
-                className={`transition-max-height duration-300 ease-in-out px-3 ${theme == 'vs-dark' ? `bg-neutral-800 text-neutral-200` : `bg-zinc-200 text-neutral-900`} remove-scrollbar font-mono border-none overflow-y-scroll ${isOpen ? 'max-h-[450px] min-h-[250px] pb-12 pt-8' : 'max-h-0 p-0'
+                className={`transition-max-height duration-300 ease-in-out px-3 ${theme == 'vs-dark' ? `bg-neutral-800 text-neutral-200` : `bg-zinc-200 text-neutral-900`} remove-scrollbar font-mono border-none overflow-y-scroll ${isOpen ? 'max-h-[450px] min-h-[250px] pb-12 pt-2' : 'max-h-0 p-0'
                     }`}
             >
-                {loading ? <p className=' w-full h-full flex items-center justify-center'>Loading <span className='blinking-dot'></span><span className='blinking-dot'></span></p> : <pre className={`w-full h-fit whitespace-pre-wrap break-words ${error ? 'text-red-500' : ' text-green-500'} `}>
-                    {data
-                        ? `${data} \n ${Time}`
-                        : error
-                        && `${error}`}
-                    <span className="blinking-cursor"></span>
-                </pre>}
+                <div className='w-full text-right pr-5 h-4'>{`${Time} `}</div>
+                <div className={`w-full h-full max-h-full min-h-[350px] p-4 mt-4 rounded-lg ${theme == 'vs-dark' ? `bg-neutral-900 ` : ' bg-neutral-400 '} py-10 box-border   `}>
+                    {loading ? <p className=' w-full h-full flex items-center justify-center'>Loading <span className='blinking-dot'></span><span className='blinking-dot transition delay-100'></span></p> :
+                        <pre className={`w-full h-fit whitespace-pre-wrap break-words ${error ? 'text-red-500' : ' text-grey-300'} `}>
+                            {data
+                                ? `${data} `
+                                : error
+                                && `${error}`}
+                            <span className="blinking-cursor"></span>
+                        </pre>}
+                </div>
             </div>
 
 
